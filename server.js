5fb20973c8a17a2782c37130a9eab71499dfc0c0
Ian <ian.k.carson@gmail.com>
2020-08-26 15:36:28 -0400

    fix heroku db path pt2

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((err, req, res, next)=> {
 
 const init = async()=> {
   try {
-    const port = (PROCESS.ENV.PORT || 3000);
+    const port = (process.env.PORT || 3000);
     app.listen(port, ()=> console.log(`listening on port ${port}`));
   }
   catch(ex){
