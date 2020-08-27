06ff8a1d28c784254059ff9b761217655e5f3307
Ian <ian.k.carson@gmail.com>
2020-08-26 20:20:29 -0400

    try haroku v4

--- src/AcmeColumn.js
+++ src/AcmeColumn.js
@@ -1,50 +1,50 @@
 import React, { Component } from 'react';
-//import AcmeElement from './AcmeElement';
+import AcmeElement from './AcmeElement';
 
 
 
-class AcmeColumn extends Component{
-    constructor(props){
-        console.log(props);
-        super();
-        this.state = {
-            employees : [],
-            departments : []
-        }
-    }
-
-    componentDidMount(){
-        //this.setState({employees:props.employees});
-    }
-
-    render(){
-        return(
-            <div>hello
-                {console.log(this.state.employees)}
-            </div> 
-            
-        )
-    }
-}
-
-// const AcmeColumn = ({departments, employees}) =>{
-//     //probably should've done this type of mapping in my router with different querying?
-//     const obj = {};
-//     departments.forEach(dept => obj[`${dept.department}`]=employees.reduce((acc,employee)=>{
-//         if(employee.department===dept.department){
-//             acc.push(employee);
+// class AcmeColumn extends Component{
+//     constructor(props){
+//         console.log(props);
+//         super();
+//         this.state = {
+//             employees : [],
+//             departments : []
 //         }
-//         return acc;
-//     },[]));
-//     return(
-//         Object.keys(obj).map(key=>{
-//             return (
-//             <div className='childDiv' id={key}>
-//             <AcmeElement passedObj={obj[key]} /> 
-//             </div>
-//             )
-//           })
-//     );
+//     }
+
+//     componentDidMount(){
+//         //this.setState({employees:props.employees});
+//     }
+
+//     render(){
+//         return(
+//             <div>hello
+//                 {console.log(this.state.employees)}
+//             </div> 
+            
+//         )
+//     }
 // }
+
+const AcmeColumn = ({departments, employees}) =>{
+    //probably should've done this type of mapping in my router with different querying?
+    const obj = {};
+    departments.forEach(dept => obj[`${dept.department}`]=employees.reduce((acc,employee)=>{
+        if(employee.department===dept.department){
+            acc.push(employee);
+        }
+        return acc;
+    },[]));
+    return(
+        Object.keys(obj).map(key=>{
+            return (
+            <div className='childDiv' id={key}>
+            <AcmeElement passedObj={obj[key]} /> 
+            </div>
+            )
+          })
+    );
+}
 
 export default AcmeColumn;
